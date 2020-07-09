3e94d21c499d51d1ad1095a4b5f9a2249d835f6b
Ahmed Dardery <Megadardery@yahoo.com>
2020-07-09 03:25:35 +0200

    rewritten

--- UVa/11294 - Wedding.cpp
+++ UVa/11294 - Wedding.cpp
@@ -1,116 +1,150 @@
 #include <bits/stdc++.h>
-int tsts;
 
+#define endl '\n'
 using namespace std;
-typedef long long  ll;
-const int N = 120, M = N * N + N;
+typedef long long ll;
+const int N = 2 * 61, M = 4 * N + 2 * N * N;
 
-int head[N], to[M], nxt[M], ne;
-void add_edge(int f, int t) {
-	nxt[ne] = head[f];
-	to[ne] = t;
-	head[f] = ne++;
+struct ADJ {
+    int head[N], nxt[M], from[M], to[M], n, ne;
+
+    void add_edge(int f, int t) {
+        from[ne] = f;
+        to[ne] = t;
+        nxt[ne] = head[f];
+        head[f] = ne++;
+    }
+
+    void init(int nNodes) {
+        ne = 0;
+        n = nNodes;
+        memset(head, -1, n * sizeof head[0]);
+    }
+};
+
+struct SCC {
+    ADJ adj;
+
+    int visidx[N], lowlink[N], cmp[N], stck[N], stcksz, cmpid, dfst;
+
+    void operator()() {
+        int n = adj.n;
+        memset(visidx, -1, n * sizeof visidx[0]);
+        memset(lowlink, -1, n * sizeof lowlink[0]);
+        memset(cmp, -1, n * sizeof cmp[0]);
+        cmpid = dfst = stcksz = 0;
+        for (int i = 0; i < n; i++) {
+            if (visidx[i] == -1)
+                dfs(i);
+        }
+    }
+
+    void dfs(int u) {
+        visidx[u] = lowlink[u] = dfst++;
+        stck[stcksz++] = u;
+
+        for (int e = adj.head[u], v; ~e && (v = adj.to[e], 1); e = adj.nxt[e]) {
+            if (visidx[v] == -1) {
+                dfs(v);
+                lowlink[u] = min(lowlink[u], lowlink[v]);
+            } else if (cmp[v] == -1)
+                lowlink[u] = min(lowlink[u], visidx[v]);
+        }
+        if (lowlink[u] == visidx[u]) {
+            do {
+                cmp[stck[--stcksz]] = cmpid;
+            } while (stck[stcksz] != u);
+            ++cmpid;
+        }
+    }
+};
+
+struct TwoSat {
+    SCC scc;
+
+    void init(int n) {
+        scc.adj.init(2 * n);
+    }
+
+    void force(int a, bool val) {
+        add_or(a, val, a, val);
+    }
+
+    void add_or(int a, bool aVal, int b, bool bVal) {
+        scc.adj.add_edge(2 * a ^ aVal, 2 * b ^ !bVal);
+        scc.adj.add_edge(2 * b ^ bVal, 2 * a ^ !aVal);
+    }
+
+    void add_xor(int a, bool aVal, int b, bool bVal) {
+        add_or(a, aVal, b, bVal);
+        add_or(a, !aVal, b, !bVal);
+    }
+
+    int value[N], nodes[N];
+
+    bool operator[](int i) {
+        return value[2 * i];
+    }
+
+    bool operator()() {
+        int n = scc.adj.n;
+
+        scc();
+        for (int i = 0; i < n; i += 2)
+            if (scc.cmp[i] == scc.cmp[i ^ 1]) return 0;
+
+        memset(value, -1, n * sizeof value[0]);
+        iota(nodes, nodes + n, 0);
+        sort(nodes, nodes + n, [this](int a, int b) { return scc.cmp[a] < scc.cmp[b]; });
+        for (int i = 0; i < n; i++) {
+            if (~value[nodes[i]]) continue;
+            value[nodes[i]] = 1;
+            value[nodes[i] ^ 1] = 0;
+        }
+        return 1;
+    }
+};
+
+int halal, haram;
+
+TwoSat twoSat;
+
+int getNode(int i, char h) {
+    return 2 * i + (h == 'h');
 }
 
-int n, m;
-
-int dfst, visidx[N], lowlink[N], cmpid[N], cmpn;
-int stck[N], stcksz = 0;
-void sccbfs(int u) {
-
-	visidx[u] = lowlink[u] = dfst++;
-	stck[stcksz++] = u;
-	for (int e = head[u], v; v = to[e], ~e; e = nxt[e]) {
-		if (visidx[v] == -1) {
-			sccbfs(v);
-			lowlink[u] = min(lowlink[u], lowlink[v]);
-		}
-		else if (cmpid[v] == -1) {	//not cross edge
-			lowlink[u] = min(lowlink[u], visidx[v]);
-		}
-	}
-	if (lowlink[u] == visidx[u]) {
-		while (stcksz--) {
-			cmpid[stck[stcksz]] = cmpn;
-			if (stck[stcksz] == u) break;
-		}
-		cmpn++;
-	}
-}
-void scc() {
-	memset(visidx, -1, n * sizeof visidx[0]);
-	memset(lowlink, -1, n * sizeof lowlink[0]);
-	memset(cmpid, -1, n * sizeof cmpid[0]);
-	cmpn = dfst = stcksz = 0;
-	for (int i = 0; i < n; i++) {
-		if (visidx[i] == -1)
-			sccbfs(i);
-	}
-}
-
-int nodes[N];
-int value[N];
-bool twosat() {
-	scc();
-	memset(value, -1, n * sizeof value[0]);
-	for (int i = 0; i < n; i += 2) {
-		if (cmpid[i] == cmpid[i ^ 1]) return 0;
-	}
-	iota(nodes, nodes + n, 0);
-	sort(nodes, nodes + n, [](int a, int b) {return cmpid[a] < cmpid[b]; });
-	for (int i = 0; i < n; i++) {
-		if (~value[nodes[i]]) continue;
-		value[nodes[i]] = 1;
-		value[nodes[i] ^ 1] = 0;
-	}
-	return 1;
-}
-
-int a, b;
-int legal, notlegal;
-void add_or(int a, int b) {
-	add_edge(a ^ 1, b);
-	add_edge(b ^ 1, a);
-}
-int getnode(char gender, int idx, bool inv) {
-	return (idx * 4 + (gender == 'h') * 2) ^ inv;
-}
-pair<int, char> toprint[N]; int tosz;
 int main() {
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "rt", stdin);
+    ios_base::sync_with_stdio(0), cin.tie(0);
+#ifdef CLION
+    freopen("input.in", "rt", stdin);
 #endif
-	while (scanf("%d%d", &legal, &notlegal), legal) {
-		n = legal * 4;
-		memset(head, -1, n * sizeof head[0]);
-		ne = 0;
-		add_or(getnode('w', 0, 1), getnode('w', 0, 1));		//w0' + w0'		//thus w0 must be 0
+    while (scanf("%d%d", &halal, &haram), haram || halal) {
 
-		for (int i = 0; i < legal; i++) {
-			//(wi + hi)(wi' + hi')
-			add_or(getnode('w', i, 0), getnode('h', i, 0));
-			add_or(getnode('w', i, 1), getnode('h', i, 1));
-		}
-		for (int i = 0; i < notlegal; i++) {
-			int i1, i2;
-			char c1, c2;
-			scanf("%d%c%d%c", &i1, &c1, &i2, &c2);
-			//(a' + b')
-			add_or(getnode(c1, i1, 1), getnode(c2, i2, 1));
-		}
-		if (twosat()) {
-			tosz = 0;
-			for (int i = 2; i < n; i += 2) {
-				int j = (i >> 1);
-				if (value[i] == 0)
-					toprint[tosz++] = { j / 2,"wh"[j & 1] };
-			}
-			for (int i = 0; i < tosz; i++) {
-				printf("%d%c%c", toprint[i].first, toprint[i].second, " \n"[i == tosz - 1]);
-			}
-		}
-		else {
-			puts("bad luck");
-		}
-	}
+        int n = halal * 2;
+        twoSat.init(n);
+
+        for (int i = 0; i < haram; ++i) {
+            int a, b;
+            char ah, bh;
+            scanf("%d%c %d%c", &a, &ah, &b, &bh);
+            twoSat.add_or(getNode(a, ah), 1, getNode(b, bh), 1);
+        }
+        for (int i = 0; i < halal; ++i) {
+            twoSat.add_xor(getNode(i, 'h'), 1, getNode(i, 'w'), 1);
+        }
+        twoSat.force(getNode(0, 'w'), 1);
+
+        if (!twoSat()) {
+            puts("bad luck");
+        } else {
+            vector<string> toPrint;
+            for (int i = 2; i < n; ++i)
+                if (twoSat[i])
+                    toPrint.emplace_back(to_string(i / 2) + "wh"[i & 1]);
+
+            for (int i = 0; i < (int) toPrint.size(); ++i) {
+                printf("%s%c", toPrint[i].c_str(), " \n"[i + 1 == toPrint.size()]);
+            }
+        }
+    }
 }
